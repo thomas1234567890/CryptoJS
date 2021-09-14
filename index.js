98aa98517c8c8494a43f1da02784e371d027272f
thomas1234567890 <tompatcon@gmail.com>
2021-09-14 16:57:22 +0100

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-import './style.css'
+
+
+
+//// Get the text from _text_input and hash it ✔️
+//// Then log the hashed value ✔️
+//// Then decrypt the hashed value ✔️
+//// Then log the decrypt value ✔️
+import './style.css';
+const CryptoJS = require('./_offline_crypto');
+
+(function() {
+
+  //// hash one value then decrypt it
+  document.getElementById('_btn_hash').addEventListener('click', () => {
+    let _text = document.getElementById('_text').value.toString();
+
+    if (_text != '') {
+      console.log(`start : ${_text}`)
+      var encrypted = CryptoJS.AES.encrypt(`${_text}`, "Secret Passphrase");
+​       console.log(`encrypted : ${encrypted.toString()}`)
+      var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+      console.log(`decrypted : ${decrypted.toString(CryptoJS.enc.Utf8)}`)
+    }
+  });
+
+})();
